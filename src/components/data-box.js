49fd02874d25a9f1ab2260410304e51923d30b6d
Alex Jeffers <alex@gluestudio.co.uk>
2025-05-01 12:22:18 +0100

    updated data again

--- src/components/data-box.js
+++ src/components/data-box.js
@@ -30,8 +30,7 @@ class DataBox extends React.Component{
                     <hr/>
                     <p>Natural Environment: <span className="result">{val[0].NE}</span></p>
                     <p>Wellbeing: <span className="result">{val[0].WB}</span></p>
-                    <p>Heritage / Culture: <span className="result">{val[0].HC}</span></p>
-                    <p>Schools, Employment and Connectivity: <span className="result">{val[0].SCH}</span></p>
+                    <p>Employment / Connectivity: <span className="result">{val[0].EC}</span></p>
                     <p>House Price Quality and Value: <span className="result">{val[0].HP}</span></p>
                     {/*<p>Overall Ranking: </p>*/}
                     <hr/>
